c1901215cd9b5b13dfd0674f07896fd4d7406b31
stmamo <stmamo@mum.edu>
2019-11-07 23:11:05 -0600

    user component- start up

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Product } from '../model/Product'
+import { ProductService } from '../Services/product.service'
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private productService: ProductService) {
+    this.productForm = this.fb.group({
+      'name': ['', Validators.required],
+      'vendor': ['', Validators.required],
+      'price': ['', Validators.required],
+      'category': ['', Validators.required],
+      'imageUrl': ['', Validators.required],
+      'availableInventory': ['', Validators.required],
+      'unit': ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
   }
+
+}
+
+
+///
+
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  productForm: FormGroup;
+  product: Product;
+
+
+
+  ngOnInit() {
+  }
+
+  submitForm() {
+    this.product = this.productForm.value;
+    this.productService.registerProduct(this.product).subscribe(data => console.log(data))
+    this.productForm.reset();
+  }
+
 
 }
