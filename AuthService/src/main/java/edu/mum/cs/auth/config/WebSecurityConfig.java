4b6218d847e2ac378db5059bbca8ef26a00e28ca
Timothy <hiitu7even@gmail.com>
2019-11-11 21:58:47 +0300

    changes auth more

--- AuthService/src/main/java/edu/mum/cs/auth/config/WebSecurityConfig.java
+++ AuthService/src/main/java/edu/mum/cs/auth/config/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/*").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
